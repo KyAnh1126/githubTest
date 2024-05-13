1983c4e04850db923e5ebf8420d1b1e3b0ed1e08
KyAnh1126 <anh261104@gmail.com>
2024-05-13 09:54:39 +0700

    Create Test.cpp
    Create my first c++ file.

---
+++ Test.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define mod (ll)(1e9 + 7)
+#define cout(m) cout << m << "\n"
+#define cinarr(a, n) for(int i = 0; i < n; i ++) cin >> a[i];
+#define coutarr(a, n) for(int i = 0; i < n; i ++) cout << a[i] << " "; cout("");
+
+void solve() {
+    
+}
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    int tt = 1;
+    //cin >> tt;
+    while(tt--) solve();
+}
